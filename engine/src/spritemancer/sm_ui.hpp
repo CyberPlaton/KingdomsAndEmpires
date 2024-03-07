fdc886b6b8f9a779af03d363f39e98b1350cb88f
bstrohonov <bogdan2006@web.de>
2024-03-07 17:52:03 +0100

    wip: ui elements

--- engine/src/spritemancer/sm_ui.hpp
+++ engine/src/spritemancer/sm_ui.hpp
@@ -9,6 +9,74 @@ namespace ImGui
 	bool Spinner(const char* label, float radius, int thickness, const ImU32& color);
 }
 
+namespace ui
+{
+	namespace scope
+	{
+		class cwrap_text
+		{
+		public:
+			cwrap_text(size_t text_length) { ImGui::PushTextWrapPos(ImGui::GetFontSize() * text_length);}
+			cwrap_text(const std::string& text) { ImGui::PushTextWrapPos(ImGui::GetFontSize() * text.length());}
+			~cwrap_text() {ImGui::PopTextWrapPos();}
+		};
+
+		class cdisabled
+		{
+		public:
+			cdisabled()
+			{
+				ImGui::PushStyleColor(ImGuiCol_Text, ImGui::GetStyle().Colors[ImGuiCol_TextDisabled]);
+			}
+			~cdisabled()
+			{
+				ImGui::PopStyleColor();
+			}
+		};
+
+	} //- scope
+
+	//------------------------------------------------------------------------------------------------------------------------
+	class icontrol
+	{
+	public:
+		icontrol();
+		virtual ~icontrol() {};
+
+		bool show()
+		{
+			const auto result = show_ui();
+			show_tooltip();
+			return result;
+		}
+		operator bool() {return show();}
+
+	protected:
+		virtual bool show_ui() = 0;
+
+	private:
+		std::string m_id;
+		std::string m_title;
+		std::string m_tooltip;
+		std::string m_icon;
+		core::scolor m_icon_color;
+		bool m_active;
+
+	private:
+		void show_tooltip()
+		{
+			if (!m_tooltip.empty() && ImGui::IsItemHovered(ImGuiHoveredFlags_DelayNormal))
+			{
+				ImGui::BeginTooltip();
+				scope::cwrap_text wrap(m_tooltip.length());
+				ImGui::TextUnformatted(m_tooltip.data());
+				ImGui::EndTooltip();
+			}
+		}
+	};
+
+} //- ui
+
 namespace sm
 {
 	ImNodes::EditorContext* nodeeditor_ctx();
