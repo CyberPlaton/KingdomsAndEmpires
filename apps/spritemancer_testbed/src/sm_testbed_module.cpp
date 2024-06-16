e8e419c3d21c7596539f3641786d39138add0d4d
bstrohonov <bogdan2006@web.de>
2024-06-16 12:56:44 +0200

    adding: std allocator testing run

--- apps/spritemancer_testbed/src/sm_testbed_module.cpp
+++ apps/spritemancer_testbed/src/sm_testbed_module.cpp
@@ -296,13 +296,79 @@ void eastl_allocator_test_run(int object_count)
 }
 
 //------------------------------------------------------------------------------------------------------------------------
+void std_allocator_test_run(int object_count)
+{
+	struct Object
+	{
+		std::string name;
+		int value;
+		float velocity;
+		float mass;
+	};
+
+	//- setup data
+	const auto C_OBJECTS = object_count;
+
+	//- timing
+	core::ctimer timer;
+	core::ctimer allocation_timer;
+	core::ctimer deallocation_timer;
+	core::ctimer access_timer;
+	float allocation_time_cumulative = 0.0f;
+	float deallocation_time_cumulative = 0.0f;
+	float object_access_time_cumulative = 0.0f;
+
+	timer.start();
+	std::allocator<Object> allocator;
+
+	//- perform test with allocation and setting some data
+	for (auto i = 0u; i < C_OBJECTS; ++i)
+	{
+		//- allocation
+		allocation_timer.start();
+		void* memory = allocator.allocate(1);
+		allocation_time_cumulative += allocation_timer.microsecs();
+
+		//- object access
+		access_timer.start();
+		auto* object = core::iallocator::construct<Object>((Object*)memory);
+		object->name = fmt::to_string(i);
+		object->value = i;
+		object->velocity = i * glm::pi<float>();
+		object->mass = object->velocity * glm::pi<float>();
+		object_access_time_cumulative += access_timer.microsecs();
+
+		//- deallocation
+		deallocation_timer.start();
+		allocator.deallocate((Object*)memory, 1);
+		deallocation_time_cumulative += deallocation_timer.microsecs();
+	}
+
+	//- stop time
+	const auto ms = timer.millisecs();
+
+	logging::log_warn("-----------------------------------------------------------------------------------------");
+	logging::log_debug(fmt::format("Object Count: '{}'", C_OBJECTS));
+	logging::log_debug("Memory Capacity: '-/-'");
+	logging::log_debug("Memory Used: '-/-'");
+	logging::log_debug("Memory Peak Used: '-/-'");
+	logging::log_info(fmt::format("Total Time: '{}ms'", ms));
+	logging::log_info(fmt::format("Allocation Time: '{}us' (Total), '{}us' (Per Object)",
+		allocation_time_cumulative, allocation_time_cumulative / C_OBJECTS));
+	logging::log_info(fmt::format("Deallocation Time: '{}us' (Total), '{}us' (Per Object)",
+		deallocation_time_cumulative, deallocation_time_cumulative / C_OBJECTS));
+	logging::log_info(fmt::format("Access Time: '{}us' (Total), '{}us' (Per Object)",
+		object_access_time_cumulative, object_access_time_cumulative / C_OBJECTS));
+}
+
+//------------------------------------------------------------------------------------------------------------------------
 void allocators_test_runs()
 {
 	auto OBJECTS = 0;
 	const auto C_RUNS = 8;
 	const auto C_INCREMENT = 2;
 
-	logging::log_warn("\n--// Linear Allocator Testing Run //--------------------");
+	logging::log_warn("\n#### Linear Allocator Testing Run ##########################################");
 	OBJECTS = 2048;
 	for (auto i = 0; i < C_RUNS; ++i)
 	{
@@ -310,7 +376,7 @@ void allocators_test_runs()
 		OBJECTS *= C_INCREMENT;
 	}
 
-	logging::log_warn("\n--// Stack Allocator Testing Run //--------------------");
+	logging::log_warn("\n#### Stack Allocator Testing Run ###########################################");
 	OBJECTS = 2048;
 	for (auto i = 0; i < C_RUNS; ++i)
 	{
@@ -319,7 +385,7 @@ void allocators_test_runs()
 		OBJECTS *= C_INCREMENT;
 	}
 
-	logging::log_warn("\n--// Pool Allocator Testing Run //--------------------");
+	logging::log_warn("\n#### Pool Allocator Testing Run ############################################");
 	OBJECTS = 2048;
 	for (auto i = 0; i < C_RUNS; ++i)
 	{
@@ -328,11 +394,20 @@ void allocators_test_runs()
 		OBJECTS *= C_INCREMENT;
 	}
 
-	logging::log_warn("\n--// EASTL Allocator Testing Run //--------------------");
+	logging::log_warn("\n#### EASTL Allocator Testing Run ###########################################");
 	OBJECTS = 2048;
 	for (auto i = 0; i < C_RUNS; ++i)
 	{
 		eastl_allocator_test_run(OBJECTS);
+
+		OBJECTS *= C_INCREMENT;
+	}
+
+	logging::log_warn("\n#### STD Allocator Testing Run ############################################");
+	OBJECTS = 2048;
+	for (auto i = 0; i < C_RUNS; ++i)
+	{
+		std_allocator_test_run(OBJECTS);
 
 		OBJECTS *= C_INCREMENT;
 	}
