be0f731cb49934e473ccbd54c7ae3e8f62fcd4ff
bstrohonov <bogdan2006@web.de>
2024-09-01 19:39:39 +0200

    wip: vertices

--- 3rdparty/spritemancer/src/detail/sm_vertices.hpp
+++ 3rdparty/spritemancer/src/detail/sm_vertices.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <core.h>
 #include "bgfx_integration/bgfx.hpp"
+#include "sm_config.hpp"
 
 namespace sm
 {
@@ -39,15 +40,119 @@ namespace sm
 
 	//- Responsible for storing vertices and operating on them.
 	//------------------------------------------------------------------------------------------------------------------------
+	template<typename TVertex>
 	class cvertices final
 	{
 	public:
-		cvertices();
-		explicit cvertices(const bgfx::VertexLayout& layout);
+		explicit cvertices(const vertex_type type, const bgfx::VertexLayout& layout, const bgfx::VertexLayoutHandle handle,
+			const unsigned reserve = 0, const unsigned flags = BGFX_BUFFER_NONE);
 		~cvertices();
 
+		void bind(uint8_t stream = 0, unsigned vertex_start = 0, unsigned vertex_count = MAX(unsigned));
+
+		cvertices& push(TVertex&& vertex);
+		template<typename... ARGS>
+		cvertices& push(ARGS&&... args);
+
+		unsigned size() const { return static_cast<unsigned>(m_vertices.size()); }
+
 	private:
+		bgfx::VertexLayout& m_layout;
+		vector_t<TVertex> m_vertices;
+		bgfx::VertexBufferHandle m_handle;
+		bgfx::VertexLayoutHandle m_layout_handle;
+		unsigned m_flags;
 		vertex_type m_type;
 	};
+
+	//------------------------------------------------------------------------------------------------------------------------
+	template<typename TVertex>
+	sm::cvertices<TVertex>::~cvertices()
+	{
+		bgfx::destroy(m_handle);
+	}
+
+	//------------------------------------------------------------------------------------------------------------------------
+	template<typename TVertex>
+	void sm::cvertices<TVertex>::bind(uint8_t stream /*= 0*/, unsigned vertex_start /*= 0*/, unsigned vertex_count /*= MAX(unsigned)*/)
+	{
+		if (size() > 0)
+		{
+			const auto count = size();
+
+			switch (m_type)
+			{
+			default:
+			case vertex_type_none:
+			{
+				return;
+			}
+			//- For static and dynamic vertex buffers only set is required, creation is done somewhere else
+			case vertex_type_static:
+			{
+				if (!bgfx::isValid(m_handle))
+				{
+
+
+					m_handle = bgfx::createVertexBuffer(bgfx::makeRef(m_vertices, size()), m_layout, m_flags);
+				}
+				break;
+			}
+			case vertex_type_dynamic:
+			{
+				if (!bgfx::isValid(m_handle))
+				{
+					m_handle = bgfx::createDynamicVertexBuffer(bgfx::makeRef(m_vertices, size()), m_layout, m_flags);
+				}
+				else
+				{
+					bgfx::update(m_handle, 0, bgfx::makeRef(m_vertices, size()));
+				}
+				break;
+			}
+			case vertex_type_transient:
+			{
+				if (count < bgfx::getAvailTransientVertexBuffer(count, m_layout))
+				{
+					bgfx::allocTransientVertexBuffer(&buffer, count, m_layout);
+
+					auto* verts = (TVertex*)buffer.data;
+
+					bx::memCopy(verts, m_vertices.data(), count * sizeof(TVertex));
+
+					m_handle = buffer.handle;
+				}
+				break;
+			}
+			}
+
+			bgfx::setVertexBuffer(stream, m_handle, vertex_start, vertex_count, m_layout_handle);
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------------------------------
+	template<typename TVertex>
+	cvertices<TVertex>& sm::cvertices<TVertex>::push(TVertex&& vertex)
+	{
+		m_vertices.emplace_back(std::move(vertex));
+		return *this;
+	}
+
+	//------------------------------------------------------------------------------------------------------------------------
+	template<typename TVertex>
+	template<typename... ARGS>
+	cvertices<TVertex>& sm::cvertices<TVertex>::push(ARGS&&... args)
+	{
+		m_vertices.emplace_back(TVertex::make(std::forward<ARGS>(args)...));
+		return *this;
+	}
+
+	//------------------------------------------------------------------------------------------------------------------------
+	template<typename TVertex>
+	sm::cvertices<TVertex>::cvertices(const vertex_type type, const bgfx::VertexLayout& layout, const bgfx::VertexLayoutHandle handle,
+		const unsigned reserve /*= 0*/, const unsigned flags /*= BGFX_BUFFER_NONE*/) : m_layout(layout), m_type(type), m_layout_handle(handle), m_flags(flags)
+	{
+		m_vertices.reserve(reserve);
+	}
 
 } //- sm
