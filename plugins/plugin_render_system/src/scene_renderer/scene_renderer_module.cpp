5fdab5b88a4f7fb4a65c36a2a0712560021e72ab
bstrohonov <bogdan2006@web.de>
2024-10-15 10:00:34 +0200

    minor style checks

--- plugins/plugin_render_system/src/scene_renderer/scene_renderer_module.cpp
+++ plugins/plugin_render_system/src/scene_renderer/scene_renderer_module.cpp
@@ -8,10 +8,10 @@ RTTR_REGISTRATION
 {
 	using namespace render_system;
 
-rttr::registration::class_<cscene_render_module>("cscene_render_module")
-.constructor<flecs::world&>()
-(
-	rttr::policy::ctor::as_raw_ptr
+	rttr::registration::class_<cscene_render_module>("cscene_render_module")
+	.constructor<flecs::world&>()
+	(
+			rttr::policy::ctor::as_raw_ptr
 	)
 	;
 }
